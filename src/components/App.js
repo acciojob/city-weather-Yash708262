e0f89eef46701cdd51f78893879162cb9d7dabb4
Yash708262 <yash708262@gmail.com>
2025-09-21 00:38:55 +0530

    dsfs

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
-import React from "react";
-import './../styles/App.css';
+import React,{ useState, useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const API_KEY = "985cb0c563a4a40ad654ecb25361aa98";
 
-export default App
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (!query) return;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, [query]);
+
+  return (
+    <div className="weather">
+      <input
+        className="search"
+        placeholder="Enter city"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {data && data.main && data.weather && (
+        <>
+          <p>City: {data.name}</p>
+          <p>Temp: {data.main.temp}°C</p>
+          <p>{data.weather[0].description}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
